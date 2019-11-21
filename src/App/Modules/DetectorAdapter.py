24378b63a94aa88dcd77c1b7b7d646ed17cc4d60
Paweł Szymichowski <p.szymichowski@protonmail.com>
2019-11-21 14:16:44 +0100

    Added a simple mock adapter for detector module

---
+++ src/App/Modules/DetectorAdapter.py
@@ -0,0 +1,38 @@
+from time import sleep
+from os import listdir
+from PIL.Image import Image as PILImage, open as PILOpen
+from random import sample
+
+class DetectorAdapter:
+
+    def __init__(self, on_frame_loaded_callback, on_hand_detected_callback):
+
+        self._on_frame_loaded_callback = on_frame_loaded_callback
+        self._on_hand_detected_callback = on_hand_detected_callback
+
+
+class DetectorAdapterMockup(DetectorAdapter):
+
+    mock_images_path = './MockupHandImages'
+    mock_images = []
+
+    def __init__(self, on_frame_loaded_callback, on_hand_detected_callback):
+        super().__init__(on_frame_loaded_callback, on_hand_detected_callback)
+
+        # Load all images from mock images path to be displayed
+        img_name_list = listdir(self.mock_images_path)
+
+        for img_name in img_name_list:
+            img_path = self.mock_images_path + '/' + img_name
+            self.mock_images.append(PILOpen(img_path))
+
+        self.module_loop()
+
+    def module_loop(self):
+
+        while True:
+            sleep(2)
+
+            img_to_send = sample(self.mock_images, 1)
+
+            self._on_hand_detected_callback(img_to_send)
