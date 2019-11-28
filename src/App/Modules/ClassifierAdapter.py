5137cea5d268830ddfa6b4b895fe4501998b2c97
Dubores <jakubszreder98@gmail.com>
2019-11-28 14:06:22 +0100

    Make classifier run on another thread

--- src/App/Modules/ClassifierAdapter.py
+++ src/App/Modules/ClassifierAdapter.py
@@ -1,35 +1,50 @@
 import multiprocessing
-from multiprocessing import Queue
+import queue
+from multiprocessing import Queue, Process
 import keras
 import numpy as np
 from pickle import load
 import random
 import time
 
+
 class ClassifierAdapter:
+    correct_shape = (128, 128)
 
     def __init__(self, settings):
         self._master_queue: Queue = settings['master_queue']
         self.model_path = settings['classifier_settings']['model_path']
-        self.model: keras.Model = keras.models.load_model(self.model_path)
-        self.correct_shape = (128, 128)
+        self._prediction_queue = Queue()
 
-        self._master_queue.put(('classifier_ready', None))
+        classifier_process = Process(target=self.classifier_worker)
+        classifier_process.start()
 
     def classify(self, input_tensor: np.ndarray):
-        if input_tensor.shape != self.correct_shape:
-            raise RuntimeError("Wrong tensor shape!")
+        try:
+            self._prediction_queue.put(input_tensor, block=False)
+        except queue.Full:
+            pass
 
-        input_tensor /= input_tensor.max()
-        input_tensor = input_tensor.reshape(1, 128, 128, 1)
-        prediction = self.model.predict(input_tensor)
+    def classifier_worker(self):
+        model: keras.Model = keras.models.load_model(self.model_path)
+        self._master_queue.put('classifier_ready', None)
 
-        message = (
-            'sign_classified',
-            prediction
-        )
+        while True:
+            input_tensor = self._prediction_queue.get()
+            if input_tensor.shape != self.correct_shape:
+                raise RuntimeError("Wrong tensor shape!")
 
-        self._master_queue.put(message)
+            input_tensor /= input_tensor.max()
+            input_tensor = input_tensor.reshape(1, 128, 128, 1)
+            prediction = self.model.predict(input_tensor)
+
+            message = (
+                'sign_classified',
+                prediction
+            )
+
+            self._master_queue.put(message)
+
 
 class ClassifierAdapterMockup(ClassifierAdapter):
 
